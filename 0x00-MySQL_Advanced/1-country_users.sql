630d1b8eb198c04c78ef5659d6f8bc2185092db9
NenyeIkpa <nenyeikpa@gmail.com>
2024-05-01 16:27:44 -0700

    average score

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,5 +1,4 @@
 -- script that creates a table users with the following fields:
--- id, integer, never null, auto increment and primary key
 -- email, string (255 characters), never null and unique
 -- name, string (255 characters)
 
